f7e619ae91c6c0a0db800684d942879b44bffd82
urquico <44643247+urquico@users.noreply.github.com>
2024-05-09 22:41:30 +0800

    setup: project init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-screen w-screen">
+    <Button>Click me</Button>
+  </div>
+</template>
