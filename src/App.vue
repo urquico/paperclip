0af292c9633eec59598c43c1d128437a73cc4e02
urquico <44643247+urquico@users.noreply.github.com>
2024-05-10 02:05:26 +0800

    feat: added basic game logic

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,102 @@
 <script setup lang="ts">
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Terminal } from 'lucide-vue-next';
+import { watchEffect } from 'vue';
+
+import CustomContainer from './components/CustomContainer.vue';
+import Badge from './components/ui/badge/Badge.vue';
+import { usePaperclipStore } from './store/paperclip';
+
+const paperclip = usePaperclipStore();
+
+// every 1 second, trigger paperclip.buyPaperclips
+watchEffect(() => {
+  const interval = setInterval(() => {
+    paperclip.buyPaperclips(paperclip.autoClippers);
+  }, 1000);
+
+  return () => clearInterval(interval);
+});
 </script>
 
+<!-- TODO: Refactor the code, make components from it. -->
 <template>
-  <div class="flex justify-center items-center h-screen w-screen">
-    <Button>Click me</Button>
-  </div>
+  <main class="w-screen h-screen flex flex-col gap-2 mt-4 items-center">
+    <!-- Title Header -->
+    <CustomContainer>
+      <Alert class="shadow animate-pulse">
+        <Terminal class="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription> Welcome to Paperclip Game. </AlertDescription>
+      </Alert>
+    </CustomContainer>
+
+    <!-- Paperclip -->
+    <CustomContainer>
+      <div class="flex gap-2 justify-between max-md:flex-col-reverse">
+        <Button @click="paperclip.buyPaperclip" :disabled="paperclip.wire <= 0"
+          >Make Paperclip</Button
+        >
+        <Badge variant="outline" class="max-md:p-3">
+          Paperclips: {{ paperclip.paperclip }}
+        </Badge>
+      </div>
+    </CustomContainer>
+
+    <!-- Manufacturing -->
+    <CustomContainer>
+      <Card>
+        <CardHeader>
+          <CardTitle>Manufacturing</CardTitle>
+          <CardDescription
+            >{{ paperclip.clipsPerSecond }} Clip/s per second</CardDescription
+          >
+        </CardHeader>
+        <CardContent class="flex flex-col gap-2">
+          <div class="flex gap-2">
+            <Button @click="paperclip.buyWire">Buy Wire</Button>
+
+            <Badge variant="outline" class="max-md:p-3">
+              {{ paperclip.wire }}
+            </Badge>
+          </div>
+
+          <div class="flex">
+            <Badge variant="secondary" class="max-md:p-3">
+              Cost: {{ paperclip.wireCost }}
+            </Badge>
+          </div>
+        </CardContent>
+        <CardFooter class="gap-2">
+          <div class="flex gap-2">
+            <Button @click="paperclip.buyAutoClippers"
+              >Buy Auto-Clippers</Button
+            >
+
+            <Badge variant="outline" class="max-md:p-3">
+              {{ paperclip.autoClippers }}
+            </Badge>
+          </div>
+
+          <div class="flex">
+            <Badge variant="secondary" class="max-md:p-3">
+              Cost: {{ paperclip.autoClipperCost }}
+            </Badge>
+          </div>
+        </CardFooter>
+      </Card>
+    </CustomContainer>
+
+    <!-- {{ JSON.stringify(paperclip.paperclipHistory) }} -->
+    <!-- {{ JSON.stringify(paperclip.wire) }} -->
+  </main>
 </template>
---
+++ src/store/paperclip.ts
@@ -0,0 +1,103 @@
+import { defineStore } from 'pinia';
+
+type PaperClipHistory = {
+  paperclip: number;
+  wire: number;
+  timestamp: Date;
+};
+
+type PaperclipStore = {
+  wire: number;
+  wireCost: number;
+  paperclip: number;
+  paperclipPrice: number;
+  paperclipHistory: PaperClipHistory[];
+  autoClippers: number;
+  autoClipperCost: number;
+};
+
+export const usePaperclipStore = defineStore('paperclip', {
+  state: () => {
+    return {
+      wire: 10,
+      wireCost: 10.0,
+      paperclip: 0,
+      paperclipPrice: 0.25,
+      paperclipHistory: [],
+      autoClippers: 0,
+      autoClipperCost: 100.0,
+    } as PaperclipStore;
+  },
+
+  getters: {
+    // ! BUG: The clipsPerSecond getter is not returning the correct value
+    clipsPerSecond: (state) => {
+      // return 0 if there are less than 2 elements in the paperclipHistory array (since we need at least 2 elements to compute for the clips per second
+      if (state.paperclipHistory.length < 2) return 0;
+
+      // check the timestamp of the first and last element in the paperclipHistory array
+      const first = state.paperclipHistory[0] as PaperClipHistory;
+      const last = state.paperclipHistory[
+        state.paperclipHistory.length - 1
+      ] as PaperClipHistory;
+
+      // compute for the time interval (in seconds)
+      const timeInterval =
+        (last.timestamp.getTime() - first.timestamp.getTime()) / 1000;
+
+      if (timeInterval <= 0) {
+        state.paperclipHistory = [];
+        return 0;
+      }
+
+      // compute for the clips per second
+      const cps = state.paperclipHistory.length / timeInterval;
+      return cps.toFixed(2);
+    },
+  },
+
+  actions: {
+    buyPaperclip: function (this: {
+      paperclip: number;
+      wire: number;
+      paperclipHistory: {
+        push(arg0: PaperClipHistory): unknown;
+      };
+    }) {
+      if (this.wire > 0) {
+        this.paperclip++;
+        this.wire -= 1;
+
+        this.paperclipHistory.push({
+          paperclip: this.paperclip,
+          wire: this.wire,
+          timestamp: new Date(),
+        });
+      }
+    },
+
+    buyPaperclips: function (
+      this: { paperclip: number; wire: number },
+      quantity: number,
+    ) {
+      // TODO: this should still be able to buy paperclips even if the wire is less than the quantity
+      if (this.wire >= quantity) {
+        this.paperclip += quantity;
+        this.wire -= quantity;
+      }
+    },
+
+    buyWire: function (this: { wire: number }) {
+      this.wire += 1000;
+    },
+
+    buyAutoClippers: function (this: { autoClippers: number }) {
+      this.autoClippers += 1;
+    },
+
+    flushHistory: function (this: { paperclipHistory: PaperClipHistory[] }) {
+      // flushes the paperclipHistory array
+      this.paperclipHistory = [];
+    },
+  },
+});
